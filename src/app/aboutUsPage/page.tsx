c7ed1ccc54d8fa634c010e8264236983d5ce5f5e
Jaweria321 <jaweria.ehsan123@gmail.com>
2024-03-30 23:26:02 +0500

    home page and about us page is created

---
+++ src/app/aboutUsPage/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUsPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <a href="/homePage">Home Page</a>
+    </div>
+  );
+}
+
+export default AboutUsPage;
