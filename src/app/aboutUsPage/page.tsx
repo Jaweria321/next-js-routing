ef96cf04559d41e5bdb5fa14cc0020c3fb20bd8a
Jaweria321 <jaweria.ehsan123@gmail.com>
2024-03-31 00:10:52 +0500

    styling

--- src/app/aboutUsPage/page.tsx
+++ src/app/aboutUsPage/page.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 function AboutUsPage() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <a href="/homePage">Home Page</a>
+    <div className="text-center">
+      <h1 className="text-xl font-black mt-5">About Us</h1>
+      <a
+        href="/homePage"
+        className="bg-black text-white p-2 m-auto inline-block mt-5"
+      >
+        Home Page
+      </a>
     </div>
   );
 }
--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 function HomePage() {
   return (
-    <div>
-      <h1 className="">This is Home Page</h1>
-      <a href="/aboutUsPage">About Us</a>
+    <div className="text-center">
+      <h1 className="text-xl font-black mt-5">This is Home Page</h1>
+      <a
+        href="/aboutUsPage"
+        className="bg-black text-white p-2 m-auto inline-block mt-5"
+      >
+        About Us
+      </a>
     </div>
   );
 }
